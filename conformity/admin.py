7ce56b1f5ec72e0d74378b4c68413ebc4f6548d4
pep-un <8629097+pep-un@users.noreply.github.com>
2022-05-26 11:48:08 +0200

    Add import-export module

--- conformity/admin.py
+++ conformity/admin.py
@@ -1,7 +1,62 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import *
 
-admin.site.register(Organization)
-admin.site.register(Policy)
-admin.site.register(Mesure)
-admin.site.register(Conformity)
+# Organization
+
+
+class OrganizationResources(resources.ModelResource):
+    class Meta:
+        model = Organization
+
+
+class OrganizationAdmin(ImportExportModelAdmin):
+    ressource_class = Organization
+
+
+admin.site.register(Organization, OrganizationAdmin)
+
+
+# Policy 
+
+
+class PolicyResources(resources.ModelResource):
+    class Meta:
+        model = Policy
+
+
+class PolicyAdmin(ImportExportModelAdmin):
+    ressource_class = Policy
+
+
+admin.site.register(Policy, PolicyAdmin)
+
+
+# Measure
+
+class MesureResources(resources.ModelResource):
+    class Meta:
+        model = Mesure
+
+
+class MeasureAdmin(ImportExportModelAdmin):
+    ressource_class = Mesure
+
+
+admin.site.register(Mesure, MeasureAdmin)
+
+
+# Conformity
+
+
+class ConformityResources(resources.ModelResource):
+    class Meta:
+        model = Conformity
+
+
+class ConformityAdmin(ImportExportModelAdmin):
+    ressource_class = Conformity
+
+
+admin.site.register(Conformity, ConformityAdmin)
--- oxomium/settings.py
+++ oxomium/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django_bootstrap5',
     'conformity.apps.ConformityConfig',
+    'import_export',
+    'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
