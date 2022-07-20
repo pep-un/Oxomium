3d37332b0c8b93bdf0883793cc3b8c44f456540e
Stéphane Diacquenod <stephane-github@diacquenod.fr>
2022-07-20 06:50:23 +0200

    Add major new feature to manage Audit

--- conformity/forms.py
+++ conformity/forms.py
@@ -3,14 +3,23 @@ Forms for front-end editing of Models instance
 '''
 
 from django.forms import ModelForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
 
-class ConformityForm(ModelForm):
+
+class ConformityForm(LoginRequiredMixin, ModelForm):
     class Meta:
         model = Conformity
-        fields = ['applicable', 'responsible','status', 'comment']
+        fields = ['applicable', 'responsible', 'status', 'comment']
 
-class OrganizationForm(ModelForm):
+
+class OrganizationForm(LoginRequiredMixin, ModelForm):
     class Meta:
         model = Organization
+        fields = '__all__'
+
+
+class AuditForm(LoginRequiredMixin, ModelForm):
+    class Meta:
+        model = Audit
         fields = '__all__'
