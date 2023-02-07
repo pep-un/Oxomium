7a4ba7b3bb252a2bda3c31f1f3d12dc4083b1a45
pep-un <8629097+pep-un@users.noreply.github.com>
2023-02-07 16:24:18 +1100

    Implementation of a state machine and workflow logic for action

--- conformity/forms.py
+++ conformity/forms.py
@@ -46,7 +46,32 @@ class ActionForm(LoginRequiredMixin, ModelForm):
         self.fields['create_date'].disabled = True
         self.fields['update_date'].disabled = True
 
-        if self.get_initial_for_field(self.fields['status'], 'status') is Action.Status.CANCELED.value \
-                or self.get_initial_for_field(self.fields['status'], 'status') is Action.Status.ENDED.value:
-            for key, value in self.fields.items():
+        generic_fields = ['title', 'owner', 'status', 'status_comment']
+        analyse_fields = ['organization', 'associated_conformity', 'associated_findings', 'description']
+        plan_fields = ['plan_start_date', 'plan_end_date', 'plan_comment']
+        implement_fields = ['implement_start_date', 'implement_end_date', 'implement_status', 'implement_comment']
+        control_fields = ['control_date', 'control_comment', 'control_user']
+
+        '''Disable everything and enable only the on matching the current status'''
+        for key, value in self.fields.items():
+            if key not in generic_fields:
+                self.fields[key].disabled = True
+
+        if self.get_initial_for_field(self.fields['status'], 'status') is Action.Status.ANALYSING.value:
+            for key in analyse_fields:
+                self.fields[key].disabled = False
+        elif self.get_initial_for_field(self.fields['status'], 'status') is Action.Status.PLANNING.value:
+            for key in plan_fields:
+                self.fields[key].disabled = False
+        elif self.get_initial_for_field(self.fields['status'], 'status') is Action.Status.IMPLEMENTING.value:
+            for key in implement_fields:
+                self.fields[key].disabled = False
+        elif self.get_initial_for_field(self.fields['status'], 'status') is Action.Status.CONTROLLING.value:
+            for key in control_fields:
+                self.fields[key].disabled = False
+        elif self.get_initial_for_field(self.fields['status'], 'status') is Action.Status.CANCELED.value:
+            for key in generic_fields:
+                self.fields[key].disabled = True
+        elif self.get_initial_for_field(self.fields['status'], 'status') is Action.Status.ENDED.value:
+            for key in generic_fields:
                 self.fields[key].disabled = True
