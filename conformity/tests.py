0e5998f7cbad25e1e378d88035dafd2b88083a89
pep-un <8629097+pep-un@users.noreply.github.com>
2023-02-10 20:27:32 +1100

    test of unittest

--- conformity/tests.py
+++ conformity/tests.py
@@ -1,6 +1,20 @@
-"""
-Test, not used for now
-"""
 from django.test import TestCase
+from conformity.models import Policy, Organization
 
-# Create your tests here.
+
+class ModelsTestCase(TestCase):
+    def test_policy_applicable_policy(self):
+        policy = Policy.objects.create(name="p_test")
+        organization = Organization.objects.create(name='o_test')
+
+        organization.add_conformity(policy)
+        organization.save()
+
+        self.assertEqual(str(policy), "p_test")
+
+
+class ViewsTestCase(TestCase):
+    def test_login_loads_properly(self):
+        """The login page loads properly"""
+        response = self.client.get('127.0.0.1:8000')
+        self.assertEqual(response.status_code, 200)
